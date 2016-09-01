17ad084d77a156835dfc3b247de8081ec5205d75
olexanders <aaa@ss.ty>
2016-09-01 13:12:04 +0000

    upd

--- js/level1.js
+++ js/level1.js
@@ -1,13 +1,13 @@
 run = function () {
-
+    var ts = Math.floor(new Date().getTime() / 1000);
     //https://api.twitter.com/1.1/search/tweets.json
     var data2 = {
         oauth_consumer_key: 'lDHfkIME4mAbuAmgkVBFJAYoF',
         oauth_token: '942248905-Ow2g6gvAxE8XmxmdD3H7B36xvrN6NtQBtdKjrfcf',
         oauth_signature_method: "HMAC-SHA1",
-        oauth_timestamp: "1472391764",
+        oauth_timestamp: ts,
         oauth_version: "1.0",
-        oauth_nonce: "06f097b685094a2989c960c46dc907b7",
+        oauth_nonce: "06f077b685094a29897960c46dc907b7",
         oauth_signature: "W0vNqWA6p8DrW9sWNz6dGyqUVr8%3D"
     };
 
